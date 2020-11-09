99c10dea0a9e3b82ac6defce74e62cbcceea65ed
Ubuntu <smitp8720@gmail.com>
2020-11-08 20:46:48 -0800

    Part 3 completed

--- turnin/[spate130@cs.ucr.edu]_lab6_part3.c
+++ turnin/[spate130@cs.ucr.edu]_lab6_part3.c
@@ -17,6 +17,10 @@ void ADC_init(){
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 }
 
+
+// max = 571
+// min = 63
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
@@ -25,17 +29,22 @@ int main(void) {
     
 	ADC_init();
 	unsigned short x = ADC;
-//	unsigned char my_char;
-	
+	unsigned char tempB = 0x00;
+	unsigned short max = 200;
      /* Insert your solution below */
 
 
     while (1) {
 	x = ADC;
-	unsigned char tempB = (char)x;
+
+	if (x >= max/2){
+		tempB = 0x01;
+	}
+	else{
+		tempB = 0x00;
+	}
+
 	PORTB = tempB;
-	unsigned char tempD = (char)(x >> 8);
-	PORTD = tempD;
     }
     return 1;
 }
