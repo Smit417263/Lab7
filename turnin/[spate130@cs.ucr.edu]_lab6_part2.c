4d8ed3708e863b3dbd9f9e5ff90de9cd1ae1221e
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-11-08 21:31:11 -0800

    Update [spate130@cs.ucr.edu]_lab6_part2.c

--- turnin/[spate130@cs.ucr.edu]_lab6_part2.c
+++ turnin/[spate130@cs.ucr.edu]_lab6_part2.c
@@ -1,7 +1,7 @@
 /*	Author: lab
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #7  Exercise #1
+ *	Lab Section: 023
+ *	Assignment: Lab #7  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
